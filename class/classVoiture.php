33ff2e329ee6b4e0169254a9fcb30d5392e12cbc
Louis Payette <payette1996@gmail.com>
2023-03-09 13:37:07 -0500

    work in classes

--- class/classVoiture.php
+++ class/classVoiture.php
@@ -5,12 +5,91 @@ declare(strict_types=1);
 /***********************************/
 
 class Voiture {
+    private int $id;
+    private string $marque;
+    private string $modele;
+    private string $categorie;
+    private int $nbPassager;
+    private string $image;
+    private string $description;
 
+    public function __construct() {
+
+    }
 
     // SETTERS
+    public function set_id(int $id) : void {
+        $this->id = $id;
+    }
+
+    public function set_marque(string $marque) : void {
+        $this->marque = $marque;
+    }
+
+    public function set_modele(string $modele) : void {
+        $this->modele = $modele;
+    }
+
+    public function set_categorie(string $categorie) : void {
+        $this->categorie = $categorie;
+    }
+
+    public function set_nbPassager(int $nbPassager) : void {
+        $this->nbPassager = $nbPassager;
+    }
+
+    public function set_image(string $image) : void {
+        $this->image = $image;
+    }
+
+    public function set_description(string $description) : void {
+        $this->description = $description;
+    }
 
     // GETTERS
+    public function get_id() : int {
+        return $this->id;
+    }
+
+    public function get_marque() : string {
+        return $this->marque;
+    }
+
+    public function get_modele() : string {
+        return $this->modele;
+    }
+
+    public function get_categorie() : string {
+        return $this->categorie;
+    }
+
+    public function get_nbPassager() : int {
+        return $this->nbPassager;
+    }
+
+    public function get_image() : string {
+        return $this->image;
+    }
+
+    public function get_description() : string {
+        return $this->description;
+    }
 
     // METHODS
+    public function print_voiture_info() : string {
+        $html = (string) "
+            <ul>
+                <li>ID : {$this->id}</li>
+                <li>Marque : {$this->marque}</li>
+                <li>Modèle : {$this->modele}</li>
+                <li>Catégorie : {$this->categorie}</li>
+                <li>Nombre de passagers : {$this->nbPassager}</li>
+                <li>Image : <img src='{$this->image}'></li>
+                <li>Description : {$this->description}</li>
+            </ul>
+        ";
+
+        return $html;
+    }
 }
 ?>
